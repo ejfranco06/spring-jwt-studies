c1e8fc6e79d6182b6a2a18c216d0c5f951bd45d0
Emilio Franco <ejfranco06@gmail.com>
2020-07-27 09:39:56 -0400

    Initial Commit

---
+++ src/main/java/dev/emiliofranco/springjwtstudies/SpringJwtStudiesApplication.java
@@ -0,0 +1,13 @@
+package dev.emiliofranco.springjwtstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emiliofranco/springjwtstudies/SpringJwtStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emiliofranco.springjwtstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
