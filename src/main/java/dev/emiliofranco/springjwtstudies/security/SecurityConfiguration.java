0ebf5e0e1fe9faef5024cc1acf778b9f7162acd8
Emilio Franco <ejfranco06@gmail.com>
2020-07-27 09:55:25 -0400

    Permit all test request

---
+++ src/main/java/dev/emiliofranco/springjwtstudies/security/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package dev.emiliofranco.springjwtstudies.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/test").permitAll();
+    }
+}
